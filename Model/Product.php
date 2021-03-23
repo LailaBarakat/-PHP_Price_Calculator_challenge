932b06fd8d07e07946d8f3a24c315723341520d6
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-23 14:43:33 +0100

    add id to product class

--- Model/Product.php
+++ Model/Product.php
@@ -3,13 +3,23 @@ declare(strict_types=1);
 
 Class Product {
 
+    private int $id;
     private string $name;
     private int $price;
 
-    public function __construct(string $name, int $price)
+    public function __construct(int $id, string $name, int $price)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->price = $price;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
     }
 
     /**
