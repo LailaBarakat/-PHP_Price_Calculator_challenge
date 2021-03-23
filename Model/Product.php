b6c615581a8d8044d58511ea7f2e95f3dc7d53f9
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-23 12:25:19 +0100

    add Product class

--- Model/Product.php
+++ Model/Product.php
@@ -1,1 +1,31 @@
 <?php
+declare(strict_types=1);
+
+Class Product {
+
+    private string $name;
+    private int $price;
+
+    public function __construct(string $name, int $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPrice(): int
+    {
+        return $this->price;
+    }
+
+}
