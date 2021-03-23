fd47e439411b6c893e2632417c1b207e0b115bc4
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-23 18:35:46 +0100

    add productLoader

---
+++ Model/ProductLoader.php
@@ -0,0 +1,55 @@
+<?php
+require_once 'Product.php';
+require_once 'config.php';
+
+class ProductLoader
+{
+
+    public function getALLproducts(): array
+    {
+        $productsArray = [];
+
+        try {
+
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $conn->query("SELECT * FROM product");
+            $stmt->execute();
+
+            // set the resulting array to associative
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($results as $row) {
+                $product = new product(intval($row['id']), $row['name'], $row['price']);
+                array_push($productsArray, $product);
+            }
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+
+
+        return $productsArray;
+
+    }
+
+    public function getproduct(int $id): product
+    {
+
+        try {
+
+            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $conn->query("SELECT * FROM product WHERE id = $id");
+            $result = $stmt->fetch();
+            $product = new product(intval($result['id']), $result['name'], $result['price']);
+
+        } catch (PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+        $conn = null;
+        return $product;
+    }
+
+}
