d6c357bc3604d0db0b089ec525fe802e8b7de50b
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-24 16:44:40 +0100

    modified visual part

--- View/Overview.php
+++ View/Overview.php
@@ -1,20 +1,9 @@
 <?php
+declare(strict_types=1);
 
+include_once '../View/header.php'
 ?>
 
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="CSS/style.css?v=<?php echo time(); ?>">
-    <title>Overview</title>
-</head>
-<body>
 <h1>Welcome to everything</h1>
 <div class="container">
     <div class="row justify-content-around" id="menu">
@@ -44,5 +33,4 @@
     </div>
 </div>
 
-</body>
-</html>
+<?php require 'footer.php' ?>
