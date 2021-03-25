0a0ab388552d36f6eae555a65190063e7a7a7ca0
Laila Barakat <laila.n.barakat@gmail.com>
2021-03-25 16:05:59 +0100

    changed servername from object to be loaded from config.php

--- Model/CustomerLoader.php
+++ Model/CustomerLoader.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'Customer.php';
-//require_once '../View/config.php';
+require_once 'config.php';
+
 
 class CustomerLoader
 {
@@ -10,17 +11,24 @@ class CustomerLoader
         $customersArray = [];
 
         try {
+            global $servername; global $dbname; global $username; global $password;
 
-            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $stmt = $conn->query("SELECT id, firstname, lastname, fixedDiscount, variableDiscount, groupId FROM Customer");
+            $stmt = $conn->query("SELECT id, firstname, lastname, fixed_discount, variable_discount, group_id FROM Customer");
             $stmt->execute();
 
             // set the resulting array to associative
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             foreach ($results as $row) {
-                $customer = new Customer(intval($row['id']), $row['firstname'], $row['lastname'], $row['fixedDiscount'], $row['variableDiscount'], $row['groupId']);
+                $customer = new Customer(
+                    intval($row['id']),
+                    $row['firstname'],
+                    $row['lastname'],
+                    $row['fixed_discount']==null? 0 : intval($row['fixed_discount']),
+                    $row['variable_discount']==null? 0 : intval($row['variable_discount']),
+                    $row['group_id']);
                 array_push($customersArray, $customer);
             }
         } catch (PDOException $e) {
@@ -38,12 +46,20 @@ class CustomerLoader
 
         try {
 
-            $conn = new PDO("mysql:host=$this->servername;dbname=$this->dbname", $this->username, $this->password);
+            global $servername; global $dbname; global $username; global $password;
+
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            $stmt = $conn->query("SELECT id, firstname, lastname, fixedDiscount, variableDiscount, groupId FROM Customer WHERE id = $id");
+            $stmt = $conn->query("SELECT id, firstname, lastname, fixed_discount, variable_discount, group_id FROM Customer WHERE id = $id");
             $result = $stmt->fetch();
-            $customer = new Customer(intval($result['id']), $result['firstname'], $result['lastname'], $result['fixedDiscount'], $result['variableDiscount'], $result['groupId']);
+            $customer = new Customer(
+                intval($result['id']),
+                $result['firstname'],
+                $result['lastname'],
+                $result['fixed_discount']==null? 0 : intval($result['fixed_discount']),
+                $result['variable_discount']==null? 0 : intval($result['variable_discount']),
+                $result['group_id']);
 
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
