24e598b4264e18a85eedc2e93758ddd22f9a1f2b
Nattaya Trouillard <nattaya.trouille@gmail.com>
2021-03-25 10:52:04 +0100

    modified overview bg color, add form

--- View/Overview.php
+++ View/Overview.php
@@ -4,33 +4,34 @@ declare(strict_types=1);
 include_once '../View/header.php'
 ?>
 
-<h1>Welcome to everything</h1>
-<div class="container">
-    <div class="row justify-content-around" id="menu">
-        <div class="col-4">
-            <label for="customer"></label>
-            <button name="customer" id="customer" class="btn btn-secondary dropdown-toggle">
-                Select the customer
-            </button>
-        </div>
+    <h1>Check the price</h1>
+    <form method="get">
+        <div class="">
+            <div class="row justify-content-center" id="menu">
+                <div class="col-2">
+                    <label for="customer"></label>
+                    <button name="customer" id="customer" class="btn btn-secondary dropdown-toggle">
+                        Select the customer
+                    </button>
+                </div>
 
-        <div class="col-4">
-            <button name="product" id="product" class="btn btn-secondary dropdown-toggle">
-                Select the product
-            </button>
-        </div>
-    </div>
+                <div class="col-2">
+                    <button name="product" id="product" class="btn btn-secondary dropdown-toggle">
+                        Select the product
+                    </button>
+                </div>
+            </div>
 
-    <div class="row justify-content-center" id="calculate">
-        <div class="col-4">
-            <button type="submit" name="calculate" class="btn btn-primary">Check the price</button>
+            <div class="row justify-content-center" id="calculate">
+                <div class="col-2">
+                    <button type="submit" name="calculate" class="btn btn-primary">Check the price</button>
+                </div>
+            </div>
+            <div class="row justify-content-center" id="price">
+                <div class="col-2">
+                    <span>Show the price here!!</span>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row justify-content-center" id="price">
-        <div class="col-4">
-            <span>Show the price here!!</span>
-        </div>
-    </div>
-</div>
-
+    </form>
 <?php require 'footer.php' ?>
